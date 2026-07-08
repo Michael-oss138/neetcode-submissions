b859b72801d3e5aebd4ced106bef1c65ce04b47d
Michael Nwaizu Chidera <nwaizumichael0@gmail.com>
2026-07-08 10:53:40 +0100

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        store = {
+            ')': '(',
+            ']': '[',
+            '}': '{'
+        }
+        stack = []
+
+        for i in s:
+            if i not in store:
+                stack.append(i)
+            else:
+                if len(stack) == 0:
+                    return False
+                if store[i] != stack[-1]:
+                    return False
+                store.pop()
+            return True
